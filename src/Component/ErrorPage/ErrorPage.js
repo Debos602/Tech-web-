3003770fd789d99ee0cf3d2590de43cab7086f2c
Debos <debos.das.02@gmail.com>
2023-07-17 15:21:17 +0600

    update error page

--- src/Component/ErrorPage/ErrorPage.js
+++ src/Component/ErrorPage/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            <h2 className='text-xl font-bold'>Page not found 404</h2>
+            <h2 className='text-xl font-bold text-center'>Page not found 404</h2>
         </div>
     );
 };
--- src/Component/ResetPassword/ResetPassword.js
+++ src/Component/ResetPassword/ResetPassword.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
 
 const ResetPassword = () => {
+	const { resetPassword } = useContext(AuthContext);
+	const handleResetPassword = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const email = form.email.value;
+		console.log(email);
+		resetPassword(email)
+			.then(() => {
+				alert("check your email");
+			})
+			.catch((error) => console.log(error));
+	};
 	return (
 		<div className="bg-indigo-950 py-10">
 			<div className="container mx-auto">
@@ -9,7 +22,7 @@ const ResetPassword = () => {
 					<div className="card w-full md:w-2/5 mx-auto glass p-5">
 						<div className="card-body text-white">
 							<h2 className="text-2xl font-bold">Reset Password</h2>
-							<form className="">
+							<form onSubmit={handleResetPassword} className="">
 								<div>
 									<label className="text-2xl flex" htmlFor="login">
 										Email
